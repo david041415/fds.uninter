720d440a511d0b2cc17d6f93d4fe1fcf3a7ed856
david041415 <davidrodrigues041415@gmail.com>
2025-07-07 12:57:02 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+  const mensagem = document.getElementById('mensagem');
+  mensagem.textContent = "Bem-vindo à minha página! Continue se dedicando e nunca pare de aprender!";
+}
